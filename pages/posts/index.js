e59ba4e9475f074451289b9cd4da69f8847837ad
Carson Richter <carson.ri@protonmail.com>
2022-01-02 22:02:37 -0800

    implemented dynamic posts with graphcms data. Replaced Content component with graphcms data. Other styling changes.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GraphQLClient } from 'graphql-request';
 import Image from 'next/image';
+import Link from 'next/link';
+import PostCard from '../../components/PostCard';
 
 export async function getStaticProps() {
     const graphcms = new GraphQLClient('https://api-us-west-2.graphcms.com/v2/ckxpk6xgz05zt01z6gtuqhy32/master')
@@ -17,6 +19,7 @@ export async function getStaticProps() {
                     featureImage {
                         url
                     }
+                    youTubeLink
                 }
             }
         `
@@ -35,24 +38,16 @@ const home = ({ posts }) => {
     return (
         <div className="w-11/12 max-w-6xl mx-auto">
             <div className="my-12">
-                {posts.map(({ title, date, description, featureImage, slug }) => (
-                    <div className="lg:grid grid-cols-2 pb-10 mb-14 border-b-2 border-gray-200">
-                        <a href="https://www.youtube.com/watch?v=hN0x3EGZo5c"
-                            className="cursor-pointer  hover:drop-shadow-lg"
-                        >
-                            <Image 
-                                src={featureImage.url}
-                                className="rounded-lg"
-                                width='1000'
-                                height='600'
-                            />
-                        </a>
-                        <div className="mt-2 flex flex-col lg:pl-7 lg:mt-4">
-                            <h3 className="text-md font-semibold md:text-xl lg:text-2xl">{title}</h3>
-                            <p className="font-thin text-sm md:text-base lg:text-lg mb-2">{date}</p>
-                            <p className="font-light text-sm md:text-base lg:text-lg">{description}</p>
-                        </div>
-                    </div>
+                {posts.map(({ title, date, description, featureImage, slug, youTubeLink }) => (
+                    <PostCard 
+                        key={slug} 
+                        title={title} 
+                        date={date} 
+                        description={description} 
+                        featureImage={featureImage} 
+                        slug={slug}
+                        youTubeLink={youTubeLink}
+                    />
                 ))}
             </div>
         </div>
